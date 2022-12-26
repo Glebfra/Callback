896f607e096c1fdd15df86b3706b856f0618fd3e
Glebfra <gleb.frantsuzov@yandex.ru>
2022-12-26 23:13:00 +0300

    = changed the next_step method

--- Backend/State.py
+++ Backend/State.py
@@ -19,6 +19,8 @@ class State(object):
         self.height = height
         self.width = width
         self.states.reshape((width, height))
+        self.activator_production.reshape((width, height))
+        self.activator_concentration.reshape((width, height))
 
     def next_step(self):
         for row, cols in enumerate(self.states):
@@ -31,6 +33,13 @@ class State(object):
                     self.states[row, col] = 0
                 elif self.states[row, col] == 0 and self.activator_concentration[row, col] >= self.critical_value:
                     self.states[row, col] = 1
+
+                if 0 < self.states[row, col] < self.excitation_time:
+                    self.activator_production[row, col] = 1
+                elif self.excitation_time < self.states[row, col] < self.excitation_time + self.refractory_time or self.states[row, col] == 0:
+                    self.activator_production[row, col] = 0
+
+                self.activator_concentration[row, col] += self.activator_production[row-1:row+2, col-1:col+2].sum()
         yield self.states
 
 
